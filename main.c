da8cde935a3077a625123a2eb325a76c184d682e
Jim Huang <jserv.tw@gmail.com>
2015-10-01 05:44:08 +0800

    pre-commit: ignore changes whose lines are all blank

--- main.c
+++ main.c
@@ -12,10 +12,10 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
 {
     struct timespec diff;
     if (t2.tv_nsec-t1.tv_nsec < 0) {
-        diff.tv_sec  = t2.tv_sec  - t1.tv_sec  - 1;
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
     } else {
-        diff.tv_sec  = t2.tv_sec  - t1.tv_sec;
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
     }
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
     while (fgets(line, sizeof(line), fp)) {
         while (line[i] != '\0')
             i++;
-        line[i-1] = '\0';
+        line[i - 1] = '\0';
         i = 0;
         e = append(line, e);
     }

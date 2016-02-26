82d06770849a6c8c509b693459f93a3ca9851743
Jim Huang <jserv.tw@gmail.com>
2016-02-26 19:35:13 +0800

    mention macro OPT as identifier

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,7 +3,9 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* original version */
+/* TODO: After modifying the original version, uncomment the following
+ * line to set OPT properly */
+// #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];

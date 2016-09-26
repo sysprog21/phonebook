7ffafd30ccee9698f11053719cda62299e952865
Weinux <weiiawia@gmail>
2016-09-27 00:19:07 +0800

    reduce entry size

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,34 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: implement */
+    /*original  implement */
+
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
+
+
+
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,12 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-/* #define OPT 1*/
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+/*phonebook only search lastname so we reduce the __PHONE_BOOK_ENTRY
+ *size from 136 byte to 32 byte */
+#define OPT 1
+
+
+typedef struct __PHONE_BOOK_DETAIL {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +20,13 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail;
+
+
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    detail *pDetail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 

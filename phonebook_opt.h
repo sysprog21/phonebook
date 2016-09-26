42835aa1ae18449e289e00fc3ee1c4350c41a193
Weinux <weiiawia@gmail>
2016-09-26 13:36:12 +0800

    modify phonebook_opt.h

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,7 +5,7 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+/* #define OPT 1*/
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
